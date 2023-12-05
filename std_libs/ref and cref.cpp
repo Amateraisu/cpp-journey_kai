91b794e7c7154f9fe4bbe8d58d7654f84728d014
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2023-12-05 18:51:29 +0800

    asd

--- std_libs/ref and cref.cpp
+++ std_libs/ref and cref.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 
 namespace std {
-    class ref {
+    template <typename T>
+    class reference_Wrapper {
     private:
         T* ptr;
     public:
-        ref(T& ref): ptr(&ref) {}
+        reference_Wrapper(T& ref): ptr(&ref) {}
     };
+
+    template <typename T>
+    reference_Wrapper<T> ref(const T& other) {
+        return reference_Wrapper(other);
+    }
 }
 
 
