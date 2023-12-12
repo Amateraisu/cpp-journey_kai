687ea60290bdd739e12d252d76f8d166d6c8be32
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2023-12-12 23:09:34 +0800

    asd

--- std_libs/std_function/std_function_implementation.cpp
+++ std_libs/std_function/std_function_implementation.cpp
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
-
-template <typename T>
+template<typename T>
 class func;
 
 template <typename Ret, typename ... Params>
-class func<Ret (Params...)> {
+class func<Ret(Params...)> {
 public:
-    func(Ret (*f)(Params...)): callable{std::make_unique<callable_impl<decltype(f)>>(f)} {}
-
+    func(Ret (*f)(Params...)):callable{std::make_unique<callable_impl<decltype(f)>>(f)} {}
     Ret operator () (Params ... args) {
         return callable->call(args...);
     }
-    
+
 private:
     struct callable_interface {
-        callable_interface() = default;
         virtual ~callable_interface();
         virtual Ret call(Params...) = 0;
     };
     template <typename T>
     struct callable_impl : callable_interface {
-        T callable;
-        callable_impl(T f):callable{std::move(f)}{}
+        T c;
+        callable_impl(T f):c{std::move(f)}{}
         Ret call(Params... args) {
-            return callable(args...);
+            return c(args...);
         }
     };
     std::unique_ptr<callable_interface>callable;
@@ -37,8 +34,8 @@ int f(int x, int y) {
 
 
 int main() {
-    func<int (int, int)>ptr = &f;
-    ptr(1, 2);
+    func<int (int, int)>ptr{f};
+
 }
 
 
